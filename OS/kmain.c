9cce644b425b6ad733135e551c1a735e06ca8e0a
aneyer <waterrose9@aol.com>
2015-03-13 17:07:21 -0700

    I left off on interrupt service routines for kern's code. I also adjusted some useful functions from the code like memcopy. note t_size is same as unsigned int

--- OS/kmain.c
+++ OS/kmain.c
@@ -83,6 +83,30 @@
     unsigned int type;
 } __attribute__((packed));
 
+/* Defines an IDT entry */
+struct idt_entry
+{
+    unsigned short base_lo;
+    unsigned short sel;        /* Our kernel segment goes here! */
+    unsigned char always0;     /* This will ALWAYS be set to 0! */
+    unsigned char flags;       /* Set using the above table! */
+    unsigned short base_hi;
+} __attribute__((packed));
+
+struct idt_ptr
+{
+    unsigned short limit;
+    unsigned int base;
+} __attribute__((packed));
+
+/* Declare an IDT of 256 entries. Although we will only use the
+*  first 32 entries the rest exists as a bit
+*  of a trap. If any undefined IDT entry is hit, it normally
+*  will cause an "Unhandled Interrupt" exception. Any descriptor
+*  for which the 'presence' bit is cleared (0) will generate an
+*  "Unhandled Interrupt" exception */
+struct idt_entry idt[256];
+struct idt_ptr idtp;
 
 
 /** serial_configure_baud_rate:
@@ -161,6 +185,28 @@ void log(char *buf, unsigned int len, unsigned int type) {
     serial_write(buf, len);
 }
 
+void *memcpy(void *dest, const void *src, unsigned int count)
+{
+    const char *sp = (const char *)src;
+    char *dp = (char *)dest;
+    for(; count != 0; count--){
+        *dp++ = *sp++;
+    }
+    return dest;
+}
+
+
+void *memset(void *dest, char val, unsigned int count)
+{
+// note: unsigned int is equivalent to size_t ( type of result of sizeof operator)
+    char *temp = (char *)dest;
+    for( ; count != 0; count--){
+        *temp++ = val;
+    }
+    return dest;
+}
+
+
 void encodeGdtEntry(unsigned char *target, struct gdt source)
 {
     if (source.limit > 65536) {
@@ -207,7 +253,13 @@ void gdt_load() {
 }
 
 void create_idt() {
-    unsigned int idt[512];
+ 
+    struct idt_entry idt[256];
+    struct idt_ptr idtp;
+
+    load_idt(&idtp);
+
+/**   unsigned int idt[512];
     unsigned int value = &interrupt_handler_4;
     int i = 0;
     for(i = 0; i < 21; i = i + 2) {
@@ -215,6 +267,46 @@ void create_idt() {
         idt[i + 1] = ((value & 0x0000FFFF) + 0x00080000);
     }
     load_idt(&idt);
+*/
+}
+
+/* Use this function to set an entry in the IDT. Alot simpler
+*  than twiddling with the GDT ;) */
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags)
+{
+    /*  take the
+    *  argument 'base' and split it up into a high and low 16-bits,
+    *  storing them in idt[num].base_hi and base_lo. The rest of the
+    *  fields that you must set in idt[num] are fairly self-
+    *  explanatory when it comes to setup 
+    */
+
+    /* The interrupt routine's base address */
+    idt[num].base_lo = (base & 0xFFFF); // I assume it ands the last 16 bits only
+    idt[num].base_hi = (base >> 16) & 0xFFFF; //shifts number to right so we see first 16 bits (high)
+
+    /* The segment or 'selector' that this IDT entry will use
+    *  is set here, along with any access flags 
+    */
+    idt[num].always0 = 0;
+    idt[num].sel = sel;
+    idt[num].flags = flags;
+}
+
+/* Installs the IDT */
+void idt_install()
+{
+    /* Sets the special IDT pointer up, just like in 'gdt.c' */
+    idtp.limit = (sizeof (struct idt_entry) * 256) - 1;
+    idtp.base = &idt;
+
+    /* Clear out the entire IDT, initializing it to zeros */
+    memset(&idt, 0, sizeof(struct idt_entry) * 256);
+    
+    /* Add any new ISRs to the IDT here using idt_set_gate */
+
+    /* Points the processor's internal register to the new IDT */
+    load_idt(&idtp); /* note that all idt_load should be load_idt() because that is what we called our function. */
 }
 
 /*
