62d354f071932b5bc16fe8968c9de93888edf2ee
aneyer <waterrose9@aol.com>
2015-04-16 18:19:20 -0700

    changed folder name to include (Final version)

---
+++ OS - take 2 (final version)/main.c
@@ -0,0 +1,26 @@
+// main.c -- Defines the C-code kernel entry point, calls initialisation routines.
+// Made for JamesM's tutorials 
+// Source: http://www.jamesmolloy.co.uk/tutorial_html/2.-Genesis.html
+
+#include "monitor.h"
+#include "descriptortables.h"
+#include "timer.h"
+
+int main(struct multiboot *mboot_ptr)
+{
+    // Initialise all the ISRs and segmentation
+    init_descriptor_tables();
+    // Initialise the screen (by clearing it)
+    monitor_clear();
+    // Write out a sample string
+    monitor_write("Hello, world!\n");
+
+    asm volatile("int $0x3");
+    asm volatile("int $0x4");
+
+    asm volatile("int $0x6");
+
+    init_timer(50);
+
+    return 0;
+}
